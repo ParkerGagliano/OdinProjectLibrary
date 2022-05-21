c159250ed2ca2d616ce4e7e389b825c57dcdfe4c
Parker <pjg4086@uncw.edu>
2022-05-21 14:54:27 -0400

    added grid for the shelf and randomly generated background colors for each book on shelf

--- main.js
+++ main.js
@@ -12,20 +12,24 @@ addBookButton.addEventListener('click', function() {
     console.log(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.checked)
     let z = document.createElement('p')
     z.innerText = bookTitle.value
-    createBook(bookcount, bookTitle.value, bookAuthor.value, bookPages.value, bookRead.checked)
+    createBook(bookcount, bookTitle.value, bookAuthor.value, bookPages.value, bookRead.checked, `#${Math.floor(Math.random()*16777215).toString(16)}`)
 
 })
 
-function createBook(bookcount, title, author, pages, read) {
+function createBook(bookcount, title, author, pages, read, backgroundcolor) {
     bookcount += 1
-    bookShelf.push(new Book(bookcount, title, author, pages, read))
+    bookShelf.push(new Book(bookcount, title, author, pages, read, backgroundcolor))
     renderBooks()
 }
 
 function renderBooks() {
+    testInput.innerHTML = '';
     for(let book of bookShelf) {
         let author = document.createElement('p')
         author.innerText = book.author
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        console.log(book.backgroundcolor)
+        author.style.backgroundColor = book.backgroundcolor
         testInput.appendChild(author)
     }
 
@@ -33,12 +37,13 @@ function renderBooks() {
 
 
 class Book {
-    constructor(bookcount, title, author, pages, read) {
+    constructor(bookcount, title, author, pages, read, backgroundcolor) {
         this.bookcount =  bookcount
         this.title = title
         this.author = author
         this.pages = pages
         this.read = read
+        this.backgroundcolor = backgroundcolor
     }
     
 }
